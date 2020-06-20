c98b153440a292c80b99477a4aa1e53f7a1a2c84
Łukasz Godlewski <AronieroDYami@gmail.com>
2020-06-20 16:37:36 +0200

    Creating Hero component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="container is-fluid">
-      <section class="hero is-primary">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="title">Licznik obiadów i herbat</h1>
-          </div>
-        </div>
-      </section>
+      <Hero />
       <Form />
     </div>
   );
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Licznik obiadów i herbat</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
